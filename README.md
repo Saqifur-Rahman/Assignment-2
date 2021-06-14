85fd1c4e664d73b66a323e9ceda690662ef6be6d
Saqifur-Rahman <85213128+Saqifur-Rahman@users.noreply.github.com>
2021-06-14 21:57:29 +0530

    mySome code added

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-## map()
+## some()
 
-- Without using the native “Array.prototype.map” method of JavaScript, compose a function titled "myMap" that will take in an array of 
-elements and execute a callback function on each of those elements.
-- Resource : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+- Without using the native “Array.prototype.some” method of JavaScript, compose a function titled "mySome" that will take in an array of elements and execute a callback function on each of those elements.
+- Resource : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
--- some.js
+++ some.js
@@ -5,49 +5,46 @@ function callbackFn(age) {
 }
 
 /*
-    Array.prototype.filter()
+    Array.prototype.some()
 
     Syntax:
-    filter(callbackFn)
+    some(callbackFn)
 
     Parameters:
     callbackFn - Function is a predicate, to test each element of the array. Return a value that coerces to true to keep the element, or to false otherwise.
                 It accepts three arguments:
         1. element : The current element being processed in the array.
         2. index |OPTIONAL| : The index of the current element being processed in the array.
-        3. array |OPTIONAL| : The array filter was called upon.
+        3. array |OPTIONAL| : The array some was called upon.
 */
 
-console.log("Using filter() : ")
-const new_arr = ages.filter(callbackFn)
-console.log(new_arr)
+console.log("Using some() : ")
+console.log(ages.some(callbackFn))
 
 /*
-    My map() function : myFilter()
+    My map() function : mySome()
 
     Syntax:
-    myFilter(array, callbackFn)
+    mySome(array, callbackFn)
 
     Parameters:
-    array - The array myFilter() was called upon.
+    array - The array mySome() was called upon.
     callbackFn - Function to execute on each element.
         1. element : The current element being processed in the array.
 
-    Return : An Array containing all the array elements that pass the test. If no elements pass the test it returns an empty array.
+    Return : A Boolean. Returns true if any of the elements in the array pass the test, otherwise it returns false
 */
 
-function myFilter(arr, callbackFn) {
-    var new_arr = []
-    var j = 0
+function mySome(arr, callbackFn) {
+    var result = false
     for(var i=0; i<arr.length; i++) {
         if(callbackFn(arr[i])) {
-            new_arr[j] = arr[i]
-            j = j + 1
+            result = true
+            break
         }
     }
-    return new_arr
+    return result
 }
 
-console.log("\nUsing myFilter() : ")
-const new_arr2 = myFilter(ages, callbackFn)
-console.log(new_arr2)
+console.log("\nUsing mySome() : ")
+console.log(mySome(ages, callbackFn))
